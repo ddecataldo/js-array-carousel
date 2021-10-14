cd56ae44c84204b91f90babff7081e8125bbbd9e
ddecataldo <davidedecataldo92@gmail.com>
2021-10-14 17:26:46 +0200

    Avanzamento esercizio carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 //Dichiarazione array con la lista delle immagini del carousel
-const items = [
+const immagini = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg',
@@ -8,7 +8,7 @@ const items = [
 ];
 
 //Dichiarazione array con il nome della città corrispendente al luogo dell'immagine
-const title = [
+const titolo = [
     'Svezia',
     'Svizzera',
     'Gran Bretagna',
@@ -17,10 +17,74 @@ const title = [
 ]
 
 //Dichiarazione array con la descrizione corrispendente al luogo dell'immagine
-const text = [
+const descrizione = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     'Lorem ipsum',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+// Seleziono il contenitore che deve contenere l'immagine
+const contenitoreImmagineFull = document.querySelector(".image-full");
+const contenitoreImmagineThumb = document.querySelector(".row--image-thumb");
+
+// Assegno un valore all'indice corrente
+let indiceCorrente = 0;
+
+// Dobbiamo stampare il tag img
+for (let i = 0; i < immagini.length; i++) {
+    
+    // ad ogni ciclo il contatore si incrementa e l'indice si sposta all'elemento successivo dell'array
+    const indiceImmagine = immagini[i];
+
+    let active = "";
+
+    // Se indice immagine = 0 allora inserisci la classe active
+    if (i === indiceCorrente) {
+        active = "active";
+    }
+
+    // Assegna a una variabile il tag img dell'immagine in anteprima
+    const tagImgFull = `<img class="${active}" src="${indiceImmagine}" alt="Immagine indice #${i}">`;
+    // Assegna a una variabile il tag img della thumb
+    const tagImgThumb = `<div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="image-thumb">
+                                <img src="${indiceImmagine}" alt="Immagine indice #${i}">
+                            </div>
+                        </div>`;
+
+    // Tag img inserito nell'html
+    if(active === "active") {
+        contenitoreImmagineFull.innerHTML +=  tagImgFull;
+    } else {
+        contenitoreImmagineThumb.innerHTML += tagImgThumb;
+    }
+}
+
+// Selezione le classi delle freccie dove devo intervenireù
+const arrowTop = document.querySelector(".arrow-top");
+const arrowBottom = document.querySelector(".arrow-bottom");
+
+// Cliccando sulla freccia in alto deve essere selezionata la thumb corrente e deve essere inserita come immagini di anteprima
+arrowTop.addEventListener("click", function () {
+
+    // Seleziona l'elemento con la classe active
+    const immagineAttiva = contenitoreImmagineFull.querySelector(".active");
+    // Rimuovi dall'elemento la classe active
+    immagineAttiva.classList.remove("active");
+
+    // Decremento l'indice
+    indiceCorrente--; // -1
+
+    // Se minore di 0 l'indice deve essere assegnato all'ultimo valore della lunghezza dell'array
+    if (indiceCorrente < 0){
+        indiceCorrente = immagini.length - 1;
+    }
+
+});
+
+// Cliccando sulla freccia in alto deve essere selezionata la thumb corrente e deve essere inserita come immagini di anteprima
+arrowBottom.addEventListener("click", function () {
+
+});
