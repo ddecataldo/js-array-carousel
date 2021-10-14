45edca4fcccd03d657b3f1f63b9936218bdf0450
ddecataldo <davidedecataldo92@gmail.com>
2021-10-14 14:30:30 +0200

    Struttura base

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+//Dichiarazione array con la lista delle immagini del carousel
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+//Dichiarazione array con il nome della città corrispendente al luogo dell'immagine
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+//Dichiarazione array con la descrizione corrispendente al luogo dell'immagine
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
