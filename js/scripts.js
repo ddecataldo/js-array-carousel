3e8b42cb5a83d685078ab637cb9515d55bfa8e00
ddecataldo <davidedecataldo92@gmail.com>
2021-10-15 15:19:15 +0200

    Inserimento titolo nell'immagine

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,19 @@ const descrizione = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
 
-// Seleziono il contenitore che deve contenere l'immagine
+// Seleziono il contenitore che deve contenere l'immagine in anteprima
 const contenitoreImmagineFull = document.querySelector(".image-full");
+// Seleziono la row che deve contenere il codice per le thumb
 const contenitoreImmagineThumb = document.querySelector(".row--image-thumb");
+// Seleziono il contenitore che deve contenere il titolo
+const contenitoreTitolo = document.querySelector(".title-image-full");
+// Seleziono il contenitore che deve la descrizione
+const contenitoreDescrizione = document.querySelector(".description-image-full");
 
 // Assegno un valore all'indice corrente
-let indiceCorrente = 3;
+let indiceCorrente = 0;
+let indiceCorrenteTitle = 0;
+let indiceCorrenteDescrizione = 0;
 
 // Dobbiamo stampare il tag img
 for (let i = 0; i < immagini.length; i++) {
@@ -58,6 +65,38 @@ for (let i = 0; i < immagini.length; i++) {
 
     contenitoreImmagineFull.innerHTML +=  tagImgFull;
     contenitoreImmagineThumb.innerHTML += tagImgThumb;
+}
+
+for (let x = 0; x < titolo.length; x++){
+
+    const indiceTitolo = titolo[x];
+
+    let titoloActive = "";
+
+    // Se indice immagine = 0 allora inserisci la classe active
+    if (indiceCorrenteTitle === x) { 
+        titoloActive = "active";
+    }
+
+    tagTitolo = `<h1 class="${titoloActive}">${indiceTitolo}</h1>`;
+    
+    contenitoreTitolo.innerHTML += tagTitolo;
+}
+
+for (let y = 0; y < descrizione.length; y++){
+
+    const indiceDescrizione = descrizione[y];
+
+    let descrizioneActive = "";
+
+    // Se indice immagine = 0 allora inserisci la classe active
+    if (indiceCorrenteDescrizione === y) { 
+        descrizioneActive = "active";
+    }
+
+    tagDescrizione = `<p class="${descrizioneActive}">${indiceDescrizione}</p>`;
+    
+    contenitoreDescrizione.innerHTML += tagDescrizione;
 
 }
 
@@ -73,24 +112,46 @@ arrowTop.addEventListener("click", function () {
     // Rimuovi dall'elemento la classe active
     immagineAttiva.classList.remove("active");
 
+    // Seleziona l'elemento con la classe active
+    const titoloAttivo = contenitoreTitolo.querySelector(".active");
+    console.log(titoloAttivo, "titoloAttivo")
+    // Rimuovi dall'elemento la classe active
+    titoloAttivo.classList.remove("active");
+
+    // Seleziona l'elemento con la classe active
+    const descrizioneAttiva = contenitoreDescrizione.querySelector(".active");
+    console.log(descrizioneAttiva, "descrizioneAttiva")
+    // Rimuovi dall'elemento la classe active
+    descrizioneAttiva.classList.remove("active");
+
     // Decremento l'indice
     indiceCorrente--; // -1
+    indiceCorrenteTitle--;
+    indiceCorrenteDescrizione--;
 
     // Se minore di 0 l'indice deve essere assegnato all'ultimo valore della lunghezza dell'array
     if (indiceCorrente < 0){
         indiceCorrente = immagini.length - 1;
-    } // 4
+    }
+    if (indiceCorrenteTitle < 0){
+        indiceCorrenteTitle = titolo.length - 1;
+    }
+    if (indiceCorrenteDescrizione < 0){
+        indiceCorrenteDescrizione = descrizione.lenght -1;
+    }
 
     // Devo applicare la classe active al nuovo elemento all'indice corrente
 
     // Selezione tutte le immagini presenti nel percorso css .image-full > img in modo da ricavare un "array".
     const appactive = document.querySelectorAll(".image-full > img");
+    const titactive = document.querySelectorAll(".title-image-full > h1");
+    const descactive = document.querySelectorAll(".description-image-full > p");
 
     // Ricavato l'array inserisco il mio indice e gli aggiungo la classe active
     const nuovoIndice = appactive[indiceCorrente].classList.add("active");
+    const nuovoIndiceTitolo = titactive[indiceCorrenteTitle].classList.add("active");
+    const nuovoIndiceDescrizione = descactive[indiceCorrenteDescrizione].classList.add("active");
     
-
-
 });
 
 // Cliccando sulla freccia in alto deve essere selezionata la thumb corrente e deve essere inserita come immagini di anteprima
